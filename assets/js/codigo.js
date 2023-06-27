9243386138d3504ab0e3efc5622f1afcc67cf4c8
“Josue295” <josueachor@gmail.com>
2023-06-26 21:35:35 -0300

    NavBar Transition, Carga de Info Instruments

--- assets/js/codigo.js
+++ assets/js/codigo.js
@@ -1,1 +1,26 @@
+const NavEl = document.querySelector('.nav');
 
+window = addEventListener('scroll', () => {
+
+    if(window.scrollY > 600){
+        NavEl.classList.add('nav-scrolled')
+    }
+
+    if(window.scrollY <= 600){
+        NavEl.classList.remove('nav-scrolled')
+    }
+
+} )
+
+window.addEventListener("load", () =>{
+
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader-hidden");
+
+    loader.addEventListener("transitionend", () =>{
+
+        document.body.removeChild("loader")
+
+    })
+})
