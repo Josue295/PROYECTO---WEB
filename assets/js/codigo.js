d71e685bda2cb4c20a10d8829af8fef64c9591e9
“Josue295” <josueachor@gmail.com>
2023-07-05 21:34:14 -0300

    Carrito

--- assets/js/codigo.js
+++ assets/js/codigo.js
@@ -12,15 +12,86 @@ window = addEventListener('scroll', () => {
 
 } )
 
-window.addEventListener("load", () =>{
 
-    const loader = document.querySelector(".loader");
+// -----------Array de objetos-----------
 
-    loader.classList.add("loader-hidden");
+const shopContent = document.getElementById("shop_content")
+const verCarrito = document.getElementById("verCarrito")
+const modalContainer = document.getElementById("modalContainer")
 
-    loader.addEventListener("transitionend", () =>{
 
-        document.body.removeChild("loader")
+let carrito = [];
 
+productos.forEach((product)=>{
+
+    let content = document.createElement("div")
+    content.className = "card"
+    content.innerHTML = `
+    
+        <div class="card-image"></div>
+        <div class="card-txt">
+            <h4 class="prod_nombre">${product.nombre}</h4>
+            <p class="prod_precio">$${product.precio}</p>
+        </div>
+    
+    `;
+
+    shopContent.append(content)
+
+    let comprar = document.createElement("button")
+    comprar.className = "btn_agregar";
+    comprar.innerText = "Comprar";
+
+    content.append(comprar)
+
+    comprar.addEventListener("click", () =>{
+        carrito.push({
+            id: product.id,
+            nombre: product.nombre,
+            precio: product.precio,
+        });
+        console.log(carrito);
     })
+})
+
+
+verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex"
+
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+        <h1 class="modal-header-title">Carrito</h1> 
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalBtn = document.createElement("h1");
+    modalBtn.innerText = "x";
+    modalBtn.className = "modal-header-btn"
+
+    modalBtn.addEventListener("click", () =>{
+        modalContainer.style.display = "none"
+    })
+
+    modalHeader.append(modalBtn)
+
+    carrito.forEach((product) =>{
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modal-content"
+        carritoContent.innerHTML = `
+            <h3>${product.nombre}</h3>
+            <p>$${product.precio}</p>
+        `;
+
+        modalContainer.append(carritoContent)
+    })
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+    const totalBuyin = document.createElement("div");
+    totalBuyin.className = "total-content";
+    totalBuyin.innerHTML = `Total a pagar: $${total}`;
+
+    modalContainer.append(totalBuyin)
 })
---
+++ assets/js/products.js
@@ -0,0 +1,32 @@
+
+
+const productos = [
+
+
+    {
+        id: 1,
+        nombre: "Guitarra Fender",
+        precio: 350000,
+    },
+
+    {
+        id: 2,
+        nombre: "Microfono Shure",
+        precio: 150000,
+    },
+    
+    {
+        id: 3,
+        nombre: "Teclado Yamaha",
+        precio: 380000,
+    },
+    
+    {
+        id: 4,
+        nombre: "Bateria Mapex",
+        precio: 250000,
+    },
+    
+
+
+]
