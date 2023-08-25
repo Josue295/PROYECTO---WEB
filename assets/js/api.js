788821825cdf12ae82942d5dcd7e91dc4d8bb655
“Josue295” <josueachor@gmail.com>
2023-08-25 15:00:30 -0300

    Spotify iFrame has been added

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 const topartist=document.getElementById("top-billboard")
 const topSongs = document.getElementById("top-songs")
-
+const section = document.getElementById("section")
 
   
 
@@ -11,7 +11,7 @@ async function obtenerDatos(){
     const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'bbe7c1af60mshb2a70bee48bdcb2p1467b9jsnc6d6bb6c5387',
+        'X-RapidAPI-Key': '',
         'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
     }
     };
@@ -50,11 +50,11 @@ async function obtenerDatos(){
         
             result.forEach((post) => {
                 if(s<=10){
-                    console.log(s);
-                    console.log(post.trackMetadata.trackName);
                 topSongs.innerHTML += `
-                <div class="song-rank">                 
-                    <img class"portada" src="${post.trackMetadata.displayImageUri}"></p>  
+                <div class="song-rank">
+                <a class="episode" data-spotify-id="spotify:track:5Y8F5o4lzoYFOWOPVbI5mg">                 
+                    <img class"portada" src="${post.trackMetadata.displayImageUri}"></p>
+                </a>  
                     <h3 class="rank">Ranking: ${post.chartEntryData.currentRank}</h3>    
                     <h1 class="song">${post.trackMetadata.trackName}</h1>
                     <h3 class="song-artist">${post.trackMetadata.artists[0].name}</h3
@@ -69,4 +69,92 @@ async function obtenerDatos(){
         }
     
 }
-obtenerDatos();
+/* obtenerDatos(); */
+
+function crearSongs (){
+
+    topSongs.innerHTML +=`
+    
+    <div class="song-rank">
+    <a href="#reproductor" class="episode" data-spotify-id="spotify:track:4LRPiXqCikLlN15c3yImP7">
+        <img class="portada" src="https://i.scdn.co/image/ab67616d00001e02b46f74097655d7f353caab14">
+        <p></p>
+      </a>
+      <p class="rank">Ranking: 1</p>
+      <h1 class="song">As It Was</h1>
+      <h3 class="song-artist">Harry Styles</h3>
+    </div>
+
+    <div class="song-rank">
+    <a href="#reproductor" class="episode" data-spotify-id="spotify:track:0yLdNVWF3Srea0uzk55zFn">
+        <img class="portada" src="https://i.scdn.co/image/ab67616d00001e02f429549123dbe8552764ba1d">
+        <p></p>
+        </a>
+        <p class="rank">Ranking: 2</p>
+        <h1 class="song">Flowers</h1>
+        <h3 class="song-artist">Miley Cyrus</h3>
+    </div>
+
+    <div class="song-rank">
+    <a href="#reproductor" class="episode" data-spotify-id="spotify:track:14tflGDI5nBFdIC8f06qS3">
+        <img class="portada" src="https://i.scdn.co/image/ab67616d00001e02e97256f1edaa792fff8788ba">
+        <p></p>
+        </a>
+        <p class="rank">Ranking: 3</p>
+        <h1 class="song">Never Surrender</h1>
+        <h3 class="song-artist">Ryan Ellis</h3>
+    </div>
+
+    <div class="song-rank">
+    <a href="#reproductor" class="episode" data-spotify-id="spotify:track:6FFMfUN4uDNDtSFzS4KsTF">
+        <img class="portada" src="https://i.scdn.co/image/ab67616d00001e02507fcdb4667bf361828fade5">
+        <p></p>
+        </a>
+        <p class="rank">Ranking: 4</p>
+        <h1 class="song">Need - Live</h1>
+        <h3 class="song-artist">Ryan Ellis</h3>
+    </div>
+
+    <div class="song-rank">
+    <a href="#reproductor" class="episode" data-spotify-id="spotify:track:6v3KW9xbzN5yKLt9YKDYA2">
+        <img class="portada" src="https://i.scdn.co/image/ab67616d00001e02c820f033bd82bef4355d1563">
+        <p></p>
+      </a>
+      <p class="rank">Ranking: 5</p>
+      <h1 class="song">Señorita</h1>
+      <h3 class="song-artist">Shawn Mendes</h3>
+    </div>
+
+    <div class="song-rank">
+    <a href="#reproductor" class="episode" data-spotify-id="spotify:track:1BxfuPKGuaTgP7aM0Bbdwr">
+        <img class="portada" src="https://i.scdn.co/image/ab67616d00001e02e787cffec20aa2a396a61647">
+        <p></p>
+        </a>
+        <p class="rank">Ranking: 6</p>
+        <h1 class="song">Cruel Summer</h1>
+        <h3 class="song-artist">Taylor Swift</h3>
+    </div>
+
+    <div class="song-rank">
+    <a href="#reproductor" class="episode" data-spotify-id="spotify:track:3oNnzH6hmqIGIhJ1NcHlrh">
+        <img class="portada" src="https://i.scdn.co/image/ab67616d00001e02a3cbe5aa0bcf10eb02a8c960">
+        <p></p>
+        </a>
+        <p class="rank">Ranking: 7</p>
+        <h1 class="song">CORAZÓN VACÍO</h1>
+        <h3 class="song-artist">María Becerra</h3>
+    </div>
+
+    <div class="song-rank">
+    <a href="#reproductor" class="episode" data-spotify-id="spotify:track:2kJwzbxV2ppxnQoYw4GLBZ">
+        <img class="portada" src="https://i.scdn.co/image/ab67616d00001e02dedbec8cca43642f06533476">
+        <p></p>
+        </a>
+        <p class="rank">Ranking: 8</p>
+        <h1 class="song">If the World Was Ending</h1>
+        <h3 class="song-artist">JP Saxe</h3>
+    </div>
+      `
+}
+
+crearSongs();
